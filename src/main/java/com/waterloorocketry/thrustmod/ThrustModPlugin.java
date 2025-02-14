8416a2bba320082fc8e91780d006008185f92723
vincentjguo <vincentguo04@gmail.com>
2025-02-13 19:13:22 -0500

    implement thrust logic

--- src/main/java/com/waterloorocketry/thrustmod/ThrustModPlugin.java
+++ src/main/java/com/waterloorocketry/thrustmod/ThrustModPlugin.java
@@ -1,11 +1,16 @@
 package com.waterloorocketry.thrustmod;
 
 import info.openrocket.core.aerodynamics.FlightConditions;
+import info.openrocket.core.simulation.MotorClusterState;
 import info.openrocket.core.simulation.SimulationConditions;
 import info.openrocket.core.simulation.SimulationStatus;
 import info.openrocket.core.simulation.exception.SimulationException;
 import info.openrocket.core.simulation.extension.AbstractSimulationExtension;
 import info.openrocket.core.simulation.listeners.AbstractSimulationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
 
 /**
  * Initialize the plugin.
@@ -39,7 +44,8 @@ public class ThrustModPlugin extends AbstractSimulationExtension {
 
     private class ThrustModListener extends AbstractSimulationListener {
 
-        private double Ae; // nozzle exit area TODO: How to get? Multiple boosters?
+        private static final Logger log = LoggerFactory.getLogger(ThrustModPlugin.class);
+
         private double atm; // current atmospheric pressure
         private final double atmRef; // reference atmospheric pressure
 
@@ -48,20 +54,28 @@ public class ThrustModPlugin extends AbstractSimulationExtension {
             this.atm = atmRef;
         }
 
-        public void startSimulation(SimulationStatus status) throws SimulationException {
-
-        }
-
         @Override
         public FlightConditions postFlightConditions(SimulationStatus status, FlightConditions flightConditions) throws SimulationException {
             this.atm = flightConditions.getAtmosphericConditions().getPressure();
 
             return flightConditions;
         }
-
+        // Thrust calculated here
+        // https://github.com/openrocket/openrocket/blob/bdf79b82144a9a406cff6c523d3cd550a1cd5c1c/core/src/main/java/info/openrocket/core/simulation/RK4SimulationStepper.java#L301
         @Override
         public double postSimpleThrustCalculation(SimulationStatus status, double thrust) throws SimulationException {
-            thrust += (atmRef - atm) * Ae;
+            // F_a = F_ref + (p_ref - p_inf) * Ae
+            // Calculated thrust is for all motors => sum F_ref
+            Collection<MotorClusterState> activeMotorList = status.getActiveMotors();
+            double adjustmentFactor = 0;
+
+            for (MotorClusterState motorState : activeMotorList) {
+                adjustmentFactor += motorState.getMotor().getDiameter();
+            }
+
+            log.trace("Initial thrust: {}", thrust);
+            thrust += (atmRef - atm) * adjustmentFactor;
+            log.trace("Adjusted Thrust: {}", thrust);
 
             return thrust;
         }
